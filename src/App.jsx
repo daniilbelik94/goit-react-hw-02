2073ba1fc16a934c2260b1a23dbf531c7e9070d1
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-12-04 15:36:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Description from './components/description/description';
+import Description from './components/Description/Description';
 import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
 import Notification from './components/Notification/Notification';
